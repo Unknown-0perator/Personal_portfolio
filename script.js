3fcc67f9c2a60e63f196b9fb1b02f31e02a5c0c2
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2022-10-28 18:24:54 -0700

    New UI

---
+++ script.js
@@ -0,0 +1,7 @@
+// document.querySelector(".header__nav").addEventListener("click", function (e) {
+//   e.preventDefault();
+//   if (e.target.classList.contains("header__nav--link")) {
+//     const id = e.target.getAttribute("href");
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   }
+// });
