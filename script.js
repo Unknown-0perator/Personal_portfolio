dfc717c4db4cfaf0f958830ea02abf43f3b6be46
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2022-10-29 18:39:37 -0700

    Project and Contact blueprint created

--- script.js
+++
@@ -1,7 +0,0 @@
-// document.querySelector(".header__nav").addEventListener("click", function (e) {
-//   e.preventDefault();
-//   if (e.target.classList.contains("header__nav--link")) {
-//     const id = e.target.getAttribute("href");
-//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-//   }
-// });
